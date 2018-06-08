32243da2a645384e4504c6e14d36ef577ebf96d7
Tim Black <timothy.c.black@gmail.com>
2018-06-07 17:25:47 -0700

    Revert "Revert "UI Updates""
    This reverts commit 7d1a4f6d2a8700dfe1eb972d9d9623bb603b5ed1.

--- server.R
+++ server.R
@@ -443,10 +443,13 @@ function(input, output, session) {
       mutate(year = lubridate::year(date_occ)) %>%
       rename(Mode = mode) %>%
       replace_na(list(Mode = 'MV Occupant')) %>%
+      st_set_geometry(NULL) %>%
       group_by(Mode, year) %>%
       tally() %>%
-      st_set_geometry(NULL) %>%
-      spread(year, n)  
+      bind_rows(cbind(Mode = 'Total Fatalities', year = 2017, n = count(Fatals2yr %>% st_set_geometry(NULL) %>% filter(lubridate::year(date_occ) == '2017')))) %>%
+      bind_rows(cbind(Mode = 'Total Fatalities', year = 2018, n = count(Fatals2yr %>% st_set_geometry(NULL) %>% filter(lubridate::year(date_occ) == '2018')))) %>%
+      spread(year, n) 
+
   })
   
   # HIN Reactive 
@@ -538,7 +541,7 @@ function(input, output, session) {
         color = '#F44242',
         stroke = FALSE,
         fillOpacity = 1,
-        group = 'Collisions YTD',
+        group = 'Fatalities YTD',
         popup = ~paste0('DR#: ', dr_no, '<br>',
                         'Date: ', date_occ, '<br>',
                         'Time: ', time_occ, '<br>',
@@ -562,7 +565,7 @@ function(input, output, session) {
           pal = pal,
           values = levels(factor(infrastructureR$Type))) %>%
         addLayersControl(
-          overlayGroups = c('VZ Streets', 'Collisions YTD', 'Projects - Complete'),
+          overlayGroups = c('VZ Streets', 'Fatalities YTD', 'Projects - Complete'),
           options = layersControlOptions(collapsed = TRUE)
         )
     }
@@ -904,7 +907,7 @@ function(input, output, session) {
   })
   
   output$collision_title <- renderText({
-    paste0('YTD Fatals, as of ',format(collisionsRV$curr_date, format='%m-%d'))
+    paste0('YTD Fatalities, as of ',format(collisionsRV$curr_date, format='%m-%d'))
   })
   
   output$infrastructureSummary <- renderTable({
